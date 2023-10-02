d3efff52b8d83b1569e24bf7eac1999356ce2811
Rishabh Nanawati <rishabh.nanawati7@gmail.com>
2023-10-02 21:18:06 +0000

    activity 2

--- ebook/activities/tokens_omp/activity2_tokens.cpp
+++ ebook/activities/tokens_omp/activity2_tokens.cpp
@@ -27,7 +27,7 @@ void initCounts (int num_toks, unsigned int* token_counts) {
 // count the number of appearances of each token in the data
 void countTokensElementsFirst (unsigned int num_els, unsigned int num_tokens,
                   unsigned int* elements, unsigned int* tokens, unsigned int* token_counts) {
-
+                    
     /* for all elements in the array */
     for (int el=0; el<num_els; el++) {
         /* for all tokens in the list */
@@ -43,7 +43,7 @@ void countTokensElementsFirst (unsigned int num_els, unsigned int num_tokens,
 // count the number of appearances of each token in the data
 void countTokensTokensFirst (unsigned int num_els, unsigned int num_tokens,
                              unsigned int* elements, unsigned int* tokens, unsigned int* token_counts) {
-
+            
     /* for all tokens in the list */
     for (int tok=0; tok<num_tokens; tok++) {
         /* for all elements in the array */
@@ -62,6 +62,7 @@ void omp_countTokensElementsFirst (unsigned int num_els, unsigned int num_tokens
 
     //TODO parallel for
     /* for all elements in the array */
+    #pragma omp parallel for
     for (int el=0; el<num_els; el++) {
         /* for all tokens in the list */
         for (int tok=0; tok<num_tokens; tok++) {
@@ -79,6 +80,7 @@ void omp_countTokensTokensFirst (unsigned int num_els, unsigned int num_tokens,
 
     //TODO parallel for
     /* for all tokens in the list */
+    #pragma omp parallel for
     for (int tok=0; tok<num_tokens; tok++) {
         /* for all elements in the array */
         for (int el=0; el<num_els; el++) {
@@ -96,6 +98,7 @@ void omp_countTokensElementsFirst_reduce (unsigned int num_els, unsigned int num
 
     //TODO parallel for reduction
     /* for all elements in the array */
+    #pragma omp parallel for reduction(+:token_counts[:num_tokens])
     for (int el=0; el<num_els; el++) {
         /* for all tokens in the list */
         for (int tok=0; tok<num_tokens; tok++) {
@@ -113,6 +116,7 @@ void omp_countTokensTokensFirst_reduce (unsigned int num_els, unsigned int num_t
 
     //TODO parallel for reduction
     /* for all tokens in the list */
+    #pragma omp parallel for reduction(+:token_counts[:num_tokens])
     for (int tok=0; tok<num_tokens; tok++) {
         /* for all elements in the array */
         for (int el=0; el<num_els; el++) {
@@ -131,6 +135,7 @@ void unroll_omp_countTokensElementsFirst_reduce (unsigned int num_els, unsigned 
 
     //TODO parallel for reduction 
     /* for all elements in the array */
+    #pragma omp parallel for reduction(+:token_counts[:num_tokens])
     for (int el=0; el<num_els; el++) {
         /* for all tokens in the list */
         for (int tok=0; tok<num_tokens; tok+=8) {
@@ -138,6 +143,27 @@ void unroll_omp_countTokensElementsFirst_reduce (unsigned int num_els, unsigned 
             /* update the count for the token */
             if (elements[el] == tokens[tok]) {
                 token_counts[tok]++;
+            }
+            if (elements[el] == tokens[tok + 1]) {
+                token_counts[tok + 1]++;
+            }
+            if (elements[el] == tokens[tok + 2]) {
+                token_counts[tok + 2]++;
+            }
+            if (elements[el] == tokens[tok + 3]) {
+                token_counts[tok + 3]++;
+            }
+            if (elements[el] == tokens[tok + 4]) {
+                token_counts[tok + 4]++;
+            }
+            if (elements[el] == tokens[tok + 5]) {
+                token_counts[tok + 5]++;
+            }
+            if (elements[el] == tokens[tok + 6]) {
+                token_counts[tok + 6]++;
+            }
+            if (elements[el] == tokens[tok + 7]) {
+                token_counts[tok + 7]++;
             }
         }
     }
